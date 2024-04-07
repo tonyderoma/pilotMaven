7ce52a546b54c2588e32bb5ce32dccf4865eb0cf
antcorin <antonio.corinaldi@eng.it>
2024-04-07 21:22:19 +0200

    nuova State semplificata

--- src/main/java/it/eng/pilot/State.java
+++ src/main/java/it/eng/pilot/State.java
@@ -1,12 +1,9 @@
 package it.eng.pilot;
 
-import java.util.Map;
-
 /**
  * Classe per la gestione dello stato di una applicazione per evitare il
  * passaggio continuo di parametri tra metodi. Gestisce variabili di stato
- * globali application-scoped di tipo Object generico, PList<Object> e
- * PMap<Object,Object>
+ * globali application scoped
  * 
  * @author Antonio Corinaldi
  * 
@@ -15,8 +12,6 @@ public class State {
 	private static State INSTANCE;
 	private final Pilot p = new Pilot();
 	private PMap<String, Object> mappa = p.pmap();
-	private PMap<String, PList<Object>> mappaLista = p.pmapl();
-	private PMap<String, PMap<Object, Object>> mappaMap = p.pmap();
 
 	public static State getInstance() {
 		if (INSTANCE == null) {
@@ -32,14 +27,6 @@ public class State {
 	 * @param val
 	 */
 	public void setState(String key, Object val) {
-		if (val instanceof PList) {
-			mappaLista.put(key, (PList<Object>) val);
-			return;
-		}
-		if (val instanceof PMap) {
-			mappaMap.put(key, (PMap<Object, Object>) val);
-			return;
-		}
 		mappa.put(key, val);
 	}
 
@@ -50,13 +37,7 @@ public class State {
 	 * @return Object
 	 */
 	public Object getState(String key) {
-		if (mappa.containsKey(key))
-			return mappa.get(key);
-		if (mappaLista.containsKey(key))
-			return mappaLista.get(key);
-		if (mappaMap.containsKey(key))
-			return mappaMap.get(key);
-		return null;
+		return mappa.get(key);
 	}
 
 	/**
@@ -72,12 +53,7 @@ public class State {
 	 * @param key
 	 */
 	public void clearState(String key) {
-		if (mappa.containsKey(key))
-			mappa.remove(key);
-		if (mappaLista.containsKey(key))
-			mappaLista.remove(key);
-		if (mappaMap.containsKey(key))
-			mappaMap.remove(key);
+		mappa.remove(key);
 	}
 
 	/**
@@ -87,16 +63,7 @@ public class State {
 	 * @return PMap<String, Object>
 	 */
 	public PMap<String, Object> getStateFull() {
-		PMap<String, Object> map = p.pmap();
-		for (Map.Entry<String, Object> entry : mappa.entrySet()) {
-			map.put(entry.getKey(), entry.getValue());
-		}
-		for (Map.Entry<String, PList<Object>> entry : mappaLista.entrySet()) {
-			map.put(entry.getKey(), entry.getValue());
-		}
-		for (Map.Entry<String, PMap<Object, Object>> entry : mappaMap.entrySet()) {
-			map.put(entry.getKey(), entry.getValue());
-		}
-		return map;
+		return mappa;
 	}
+
 }
