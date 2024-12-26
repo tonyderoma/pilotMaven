b368261fb59ca930a9193231288f26bce4312b5c
antcorin <antonio.corinaldi@eng.it>
2024-12-26 14:04:10 +0100

    findIndex

--- src/main/java/it/eng/pilot/PArrayList.java
+++ src/main/java/it/eng/pilot/PArrayList.java
@@ -3167,4 +3167,8 @@ public class PArrayList<K> extends ArrayList<K> implements PList<K> {
 		this.circular = circular;
 		return this;
 	}
+
+	public int findIndex() throws Exception {
+		return indexOf(findOne());
+	}
 }
--- src/main/java/it/eng/pilot/PList.java
+++ src/main/java/it/eng/pilot/PList.java
@@ -5183,4 +5183,14 @@ public interface PList<E> extends List<E> {
 	 * @return PList<E>
 	 */
 	public PList<E> circular(int n);
+
+	/**
+	 * Ritorna l'indice dell'elemento trovato all'interno della lista dopo aver
+	 * applicato le condizioni di filtro
+	 * 
+	 * @return int
+	 * @throws Exception
+	 */
+	int findIndex() throws Exception;
+
 }
